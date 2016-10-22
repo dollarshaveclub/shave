ca77b3bfd21c4bece461416a8db422ff11bbac20
Jeff Wainwright <jjwainwright2@gmail.com>
2016-10-22 00:31:08 -0700

    updates for landing page

--- main.js
+++ main.js
@@ -1,19 +1,20 @@
-$(document).ready(function(){
-  var $window = $(window);
-  var timer = null;
-  var runShave = function() {
-
-    if ($window.width() > 768) {
-      shave('#demo-text', 150, {character: '...🐔 (Shave works!)'});
-    } else {
-      shave('#demo-text', 80, {character: '...🐔 (Shave works!)'});
-    }
-  };
-  runShave();
-  $window.on('resize', function() {
-    clearTimeout(timer);
-    timer = setTimeout(function() {
-      runShave();
-    }, 1000);
-  });
-});
+var btn = document.getElementById('button');
+console.log('here', btn);
+var timer = null;
+var runShave = function() {
+  var demoText = document.getElementById('demo-text');
+  var hasShave = demoText.querySelector('.js-shave');
+  console.log(demoText, hasShave);
+  if (hasShave) {
+    shave(demoText, 1000, {character: '...🐔 (Shave works!)'});
+  } else {
+    shave(demoText, 80, {character: '...🐔 (Shave works!)'});
+  }
+};
+btn.addEventListener('click', function() {
+  console.log('test');
+  clearTimeout(timer);
+  timer = setTimeout(function() {
+    runShave();
+  }, 1000);
+}, false);
