aa99e85b154e76a66566b0f63d483a86fbf32e8f
Qisong Chen <qchen@unioncreative.com>
2017-05-15 11:06:12 -0400

    Fixed issue where text is truncated when client height is the same as max height.

--- dist/shave.js
+++ dist/shave.js
@@ -45,7 +45,7 @@ function shave(target, maxHeight, opts) {
     el.style.maxHeight = 'none';
 
     // If already short enough, we're done
-    if (el.offsetHeight < maxHeight) {
+    if (el.offsetHeight <= maxHeight) {
       el.style.height = heightStyle;
       el.style.maxHeight = maxHeightStyle;
       continue;
