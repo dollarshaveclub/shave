0ee11b1b0d3fcacb6490369d6a8a7b07b2c14a66
Alex Cross <awcross@users.noreply.github.com>
2017-07-21 10:16:40 -0700

    Update build to export ES module (#63)
    * Add `pkg.module` and update build to export ES module in addition to UMD.
    
    * Revert "Add `pkg.module` and update build to export ES module in addition to UMD."
    
    This reverts commit 647c3042de6619e8213dbffcdc31f43841db9ce1.
    
    * Update build for ES module without a breaking change.
    
    * Add `module` to package.json

---
+++ dist/shave.es.js
@@ -0,0 +1,67 @@
+function shave(target, maxHeight) {
+  var opts = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : {};
+
+  if (!maxHeight) throw Error('maxHeight is required');
+  var els = typeof target === 'string' ? document.querySelectorAll(target) : target;
+
+  var character = opts.character || '…';
+  var classname = opts.classname || 'js-shave';
+  var spaces = opts.spaces || false;
+  var charHtml = '<span class="js-shave-char">' + character + '</span>';
+
+  if (!('length' in els)) els = [els];
+  for (var i = 0; i < els.length; i += 1) {
+    var el = els[i];
+    var styles = el.style;
+    var span = el.querySelector('.' + classname);
+    var textProp = el.textContent === undefined ? 'innerText' : 'textContent';
+
+    // If element text has already been shaved
+    if (span) {
+      // Remove the ellipsis to recapture the original text
+      el.removeChild(el.querySelector('.js-shave-char'));
+      el[textProp] = el[textProp]; // nuke span, recombine text
+    }
+
+    var fullText = el[textProp];
+    var words = spaces ? fullText.split(' ') : fullText;
+
+    // If 0 or 1 words, we're done
+    if (words.length < 2) continue;
+
+    // Temporarily remove any CSS height for text height calculation
+    var heightStyle = styles.height;
+    styles.height = 'auto';
+    var maxHeightStyle = styles.maxHeight;
+    styles.maxHeight = 'none';
+
+    // If already short enough, we're done
+    if (el.offsetHeight <= maxHeight) {
+      styles.height = heightStyle;
+      styles.maxHeight = maxHeightStyle;
+      continue;
+    }
+
+    // Binary search for number of words which can fit in allotted height
+    var max = words.length - 1;
+    var min = 0;
+    var pivot = void 0;
+    while (min < max) {
+      pivot = min + max + 1 >> 1; // eslint-disable-line no-bitwise
+      el[textProp] = spaces ? words.slice(0, pivot).join(' ') : words.slice(0, pivot);
+      el.insertAdjacentHTML('beforeend', charHtml);
+      if (el.offsetHeight > maxHeight) max = spaces ? pivot - 1 : pivot - 2;else min = pivot;
+    }
+
+    el[textProp] = spaces ? words.slice(0, max).join(' ') : words.slice(0, max);
+    el.insertAdjacentHTML('beforeend', charHtml);
+    var diff = spaces ? words.slice(max).join(' ') : words.slice(max);
+
+    el.insertAdjacentHTML('beforeend', '<span class="' + classname + '" style="display:none;">' + diff + '</span>');
+
+    styles.height = heightStyle;
+    styles.maxHeight = maxHeightStyle;
+  }
+}
+
+export default shave;
