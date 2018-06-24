8f4a16faf9235a85bb3e31ddf55293686ef01e46
Jeff Wainwright <yowainwright@gmail.com>
2018-06-23 20:29:24 -0700

    [update] removes gulp

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-const gulp = require('gulp')
-const qunit = require('node-qunit-phantomjs')
-
-gulp.task('test', () => {
-  qunit('tests/index.html', { verbose: true })
-})
-
-gulp.task('default', ['test'])
