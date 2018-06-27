b712783c0f3485829cf3b3ea25c471113f95e698
Jeff Wainwright <yowainwright@gmail.com>
2018-06-27 11:55:45 -0700

    [bug] fixes words var; .split(' ') => .split('')

--- dist/shave.js
+++ dist/shave.js
@@ -36,7 +36,7 @@
       }
 
       var fullText = el[textProp];
-      var words = spaces ? fullText : fullText.split(' '); // If 0 or 1 words, we're done
+      var words = spaces ? fullText : fullText.split(''); // If 0 or 1 words, we're done
 
       if (words.length < 2) continue; // Temporarily remove any CSS height for text height calculation
 
