ce63bcf7d941889cca6752aef0f7edcef630c7bb
Daniel Rivera Guzman <drivera@xentrl.com>
2018-08-10 10:23:38 -0700

    Add types/shave

---
+++ types/index.d.ts
@@ -0,0 +1,21 @@
+/**
+* Truncates multi-line text to fit within an html element based on a set max-height. 
+* It then stores the diff of the original text string in a hidden <span> element following the visible text. 
+* This means the original text remains intact!
+*/
+export default function shave(
+    target: string | Element | Element[],
+    maxHeight: number,
+    options?: Shave.Options
+): Shave;
+
+export interface Shave {
+}
+
+export namespace Shave {
+    export interface Options {
+        classname?: string,
+        character?: string,
+        spaces?: boolean
+    }
+}
